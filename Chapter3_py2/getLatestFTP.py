12d3c6cc5f1b0de2311ae4b3e6c16501437e0538
MRNIU <a6813140@hotmail.com>
2017-12-06 13:26:32 +0800

    第六章完成部分，开始第9章

--- Chapter3_py2/getLatestFTP.py
+++ Chapter3_py2/getLatestFTP.py
@@ -1,9 +1,13 @@
+#I don't know why it isn't work
+#and has erroe.
+
+
 import ftplib
 import os
 import socket
 
 HOST='ftp.mozilla.org'
-DIRN='pub/mozilla.org/wedtools'
+DIRN='pub/mozilla.org/webtools'
 FILE='bugzilla-LATEST.tar.gz'
 
 def main():
@@ -15,3 +19,29 @@ def main():
     print '*** Connect to host "%s"'%HOST
 
     try:
+        f.login()
+    except ftplib.error_perm:
+        print 'ERROR:cannot login anonymously'
+        f.quit()
+        return
+    print "*** Logged in as 'anonymous'"
+
+    try:
+        f.cwd(DIRN)
+    except ftplib.error_perm:
+        print 'ERROR:cannot CD to "%s" folder'%DIRN
+        f.quit()
+        return
+    print "***Changed to '%s' folder"%DIRN
+
+    try:
+        f.retrbinary("RETR %s"%FILE,open(FILE,'wb').write)
+    except ftplib.error_perm:
+        print "ERROR: cannot read file '%s'"%FILE
+        os.unlink(FILE)
+    else:
+     print "***Download '%s' to CWD"%FILE
+    f.quit()
+
+if __name__=='__main__':
+    main()
